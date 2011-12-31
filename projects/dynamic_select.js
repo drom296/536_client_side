66f40dccd279e02195c37e4eb9220bf1b5871419
Pedro Juan <drom296@gmail.com>
2011-12-30 19:58:12 -0500

    Added method to display the results.

--- projects/dynamic_select.js
+++ projects/dynamic_select.js
@@ -1,13 +1,14 @@
 /**
  * @author Pedro Mass
  */
+var DEFAULT = "--Choose One--";
 
 // create the data object
 var data = new Object();
 data["start"] 	= ["Men or Women?", ["men", "women"]];
 // level 1
 data["men"] 		= ["Facial Hair?", ["hair", "no hair"]];
-data["woman"] 	= ["Blonde or Brunette?", ["blonde","brunette"]];
+data["women"] 	= ["Blonde or Brunette?", ["blonde","brunette"]];
 // level2
 data["hair"] 		= ["Complexion?", ["dark","light"]];
 data["no hair"] = ["Build?", ["big", "small"]];
@@ -48,10 +49,64 @@ function processSelect(which){
 		addChoice(which);
 	} else{
 		//create the final text node
+		showResults(which);
+	}
+}
+
+/**
+ * Gets all the choices, displays them.
+ */
+function showResults(which){
+	// get the values from the selects
+	var values = getSelVals();
+
+	// check if we received results	
+	if(!values){
+		return;
+	}
+
+	// display the results
+	
+	// create the list
+	var uElem = document.createElement("ul");
+	
+	// set up the list items
+	for(val in values){
+		val = values[val];
 		
+		// create list item
+		var li = document.createElement("li");
+		// create list item text node
+		var text = document.createTextNode(val);
+		// add text to list item
+		li.appendChild(text);
+		
+		// add li to ul
+		uElem.appendChild(li);
 	}
 	
+console.log(uElem);
+
+	// add ul to the document
+	which.parentNode.appendChild(uElem);
 }
+
+function getSelVals(){
+	// get all the select values
+	var selects = document.getElementsByTagName("select");
+	var values = new Array();
+	
+	// get the values for those selects
+	for(i=0, len=selects.length; i<len; i++){
+		if (selects[i].value === DEFAULT){
+			return null;
+		}
+		values.push(selects[i].value);
+	}
+	
+	return values;
+}
+
 
 /**
  * Creates a div, adds the question, and creates the select option element
@@ -60,8 +115,11 @@ function processSelect(which){
  */
 function addChoice(elem){
 	// grab the data
-	var choices = data[elem.value][1];
+	var options = data[elem.value][1];
 	var question = data[elem.value][0];
+
+	// add the defaul option to the options array (at the front)
+	options.unshift(DEFAULT);
 	
 	// create the question
 	// create p node
@@ -69,11 +127,42 @@ function addChoice(elem){
 	// create text node
 	question = document.createTextNode(question);
 	// attach to p node
-	pQuestion.parentNode.appendChild(question);
+	pQuestion.appendChild(question);
 	// attach p node to the document
 	elem.parentNode.appendChild(pQuestion);
 	
 	
+	// create the select option element
+	var selElem = document.createElement("select");
+	
+	// set up the options
+	for (option in options){
+		option = options[option];
+		// create the option
+		var optElem = document.createElement("option");
+		// set attributes
+		optElem.setAttribute("value", option);
+		
+		// build text
+		var textElem = document.createTextNode(option);
+		// append text to opt
+		optElem.appendChild(textElem);
+		
+		//append opt to select
+		selElem.appendChild(optElem);
+	}
+	
+	// add the event handler to the select option element
+	// does not work in IE
+	if(ieSeven){
+		selElem.setAttribute('onchange', function(){processSelect(this);});
+		
+	} else{
+  	selElem.setAttribute('onchange', 'processSelect(this);');;
+  }
+	
+	// add select to the document
+	elem.parentNode.appendChild(selElem);
 	
 }
 
@@ -87,7 +176,6 @@ function killOlderSiblings(elem){
 	
 	// while we have another sibling
 	while(sib = elem.nextSibling){
-		console.log("about to remove: " + sib)
 		// remove it
 		sib.parentNode.removeChild(sib);
 	}
