7a84b1a312ea3cda10cc5b731f331d46d75a22b0
Pedro Juan <drom296@gmail.com>
2012-01-18 21:14:09 -0500

    added submit button

--- projects/project1/dynamic_select.js
+++ projects/project1/dynamic_select.js
@@ -86,6 +86,12 @@ function addContactForm(which) {
 	message.setAttribute('name', 'uMessage');
 	form.appendChild(message);
 	form.appendChild(document.createElement('br'));
+	
+	// add submit
+	var submit = document.createElement('input');
+	submit.setAttribute('name', 'submit');
+	submit.setAttribute('type', 'submit');
+	form.appendChild(submit);
 
 	// attach form to container
 	container.appendChild(form);
