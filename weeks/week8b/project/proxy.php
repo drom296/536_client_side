cd4e3f309818a5dbfcfcb233888c26c223c2dde9
unknown <dblab@SL1520-08.(none)>
2012-02-09 13:50:02 -0500

    Finished class things

--- weeks/week8b/project/proxy.php
+++ weeks/week8b/project/proxy.php
@@ -1,18 +1,18 @@
 <?php
 error_reporting(0);
-define ('HOSTNAME', 'http://simon.ist.rit.edu:8080/Services/resources/ESD');
-if($_POST['path']){
-	$hold=explode('?',$_POST['path']);
-	$path=$hold[0];
-	$post=$hold[1]."&ip=".$_SERVER['REMOTE_ADDR'];
-	$url= HOSTNAME.$path;
+define('HOSTNAME', 'http://simon.ist.rit.edu:8080/Services/resources/ESD');
+if ($_POST['path']) {
+	$hold = explode('?', $_POST['path']);
+	$path = $hold[0];
+	$post = $hold[1] . "&ip=" . $_SERVER['REMOTE_ADDR'];
+	$url = HOSTNAME . $path;
 	$session = curl_init($url);
 	curl_setopt($session, CURLOPT_HEADER, false);
 	curl_setopt($session, CURLOPT_RETURNTRANSFER, true);
-	curl_setopt($session,CURLOPT_POST,1);
-	curl_setopt($session,CURLOPT_POSTFIELDS,$post);
-}else{
-	$url=HOSTNAME.$_GET['path'];
+	curl_setopt($session, CURLOPT_POST, 1);
+	curl_setopt($session, CURLOPT_POSTFIELDS, $post);
+} else {
+	$url = HOSTNAME . $_GET['path'];
 	$session = curl_init($url);
 	curl_setopt($session, CURLOPT_HEADER, false);
 	curl_setopt($session, CURLOPT_RETURNTRANSFER, true);
