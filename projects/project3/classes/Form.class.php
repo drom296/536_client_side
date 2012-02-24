b3e77299817638f1a44042a29632932b68e833ed
Pedro Juan <drom296@gmail.com>
2012-02-24 15:27:43 -0500

    Finished the form from the server side. Need to implement actions on server side

---
+++ projects/project3/classes/Form.class.php
@@ -0,0 +1,246 @@
+<?php
+
+class Form {
+
+	/**
+	 * Builds the textual representation of a select option element based on the
+	 * passed in $values and $texts passed in. If no texts specified, will only
+	 * the values as text
+	 *
+	 * @param values - an array of values to use for the options
+	 * @param texts [optional] - an array of values to use for the options, must be
+	 * same length as values
+	 * @param name [optional]- the name for the select
+	 * @param id [optional] - id for the select
+	 * @param class [optional] - class for the select
+	 * @param clas [optional] - boolean specifying if we allow multiple answers
+	 * @param size [optional] - integer specifying the num items displayed
+	 *
+	 * @return null if there are any errors, the select element if everything goes
+	 * well
+	 */
+	static function buildSelect($values, $texts = null, $name = null, $id = null, $class = null, $multiple = null, $size = null) {
+		$result = null;
+
+		if (is_array($values) && !empty($values)) {
+			$result = "";
+
+			// grab length of values
+			$numOptions = count($values);
+
+			// check if texts exist
+			if (is_array($texts)) {
+				// check if same length of values
+				if (count($texts) != $numOptions) {
+					// they aren't so leave the funtion
+					return null;
+				}
+
+			} else {
+				// make the texts equal the values
+				$texts = $values;
+			}
+
+			// start the select
+			$result .= "\t<select";
+			// add the name
+			if (!is_null($name) && is_string($name)) {
+				$result .= " name=\"$name\"";
+			}
+			// add the id
+			if (!is_null($id) && is_string($id)) {
+				$result .= " id=\"$id\"";
+			}
+			// add the $class
+			if (!is_null($class) && is_string($class)) {
+				$result .= " class=\"$class\"";
+			}
+			// add multiple
+			if (is_bool($multiple) && $multiple) {
+				$result .= " multiple=\"multiple\"";
+			}
+			// add the size
+			if (is_numeric($size)) {
+				$result .= " size=\"$size\"";
+			}
+
+			$result .= ">\n";
+
+			// loop thru and create the options
+			for ($i = 0; $i < $numOptions; $i++) {
+
+				$result .= "\t\t<option value=\"" . $values[$i] . "\">" . $texts[$i] . "</option>\n";
+			}
+
+			// end the select
+			$result .= "\t</select>\n";
+		}
+
+		return $result;
+	}
+
+	// Sanitize data before validation
+	static function sanitizeResults(&$results) {
+		// sanitize everything in the results
+		foreach ($results as &$result) {
+			self::sanitize_data($result);
+		}
+	}
+
+	/**
+	 * Sanitizes the passed in value, and updates it
+	 */
+	static function sanitize_data(&$var) {
+		// only need to sanitize strings
+		if (is_string($var)) {
+			$var = trim($var);
+			$var = stripslashes($var);
+			$var = strip_tags($var);
+			// $var = htmlentities($var);
+			$var = preg_replace('/\r\n/', '<br />', $var);
+		}
+	}
+
+	// make sure if the results are valid
+	static function validateResults(&$results, $tableName) {
+		$errors = "";
+
+		// validate everything
+
+		// setup database connect
+		$db = Database::getInstance();
+		// get the col info
+		$colInfo = $db -> getColInfo($tableName);
+
+		foreach ($results as $key => &$result) {
+			// get the type for this field
+			$type = $colInfo[$key]['Type'];
+			// get if nullable
+			$nullable = ($colInfo[$key]['Null'] == "YES" ? true : false);
+
+			// check if null is false
+			if (!$nullable) {
+				// check if it can be auto incremented
+				$nullable = ($colInfo[$key]['Extra'] == "auto_increment" ? true : false);
+				// check if nullable is false again
+				if (!$nullable) {
+					// check if it uses current timestamp
+					$nullable = ($colInfo[$key]['Default'] == "CURRENT_TIMESTAMP" ? true : false);
+				}
+			}
+
+			// try to validate
+			$error = self::validateField($key, $result, $type, $nullable);
+
+			if (!empty($error)) {
+				$errors .= "<p>" . $error . "</p>";
+			}
+		}
+
+		return $errors;
+	}
+
+	/**
+	 * Checks to see if the value is valid, based on the other field criteria.
+	 * 3 step validation:
+	 * 		1st - check if its allowed to be empty
+	 * 		2nd - make sure it doesnt exceed maximum length
+	 * 		3rd - make sure its the right type, if so convert to that type
+	 *
+	 *
+	 * @param fieldName 	Used to specify which field was incorrect
+	 * @param value 			Value to check validity, converted to match the type
+	 * @param nullable		Determines if field is allowed to be null
+	 *
+	 * @return empty if there were no errors, something if there was
+	 */
+	static function validateField($fieldName, &$value, $type, $nullable) {
+		// setup error message
+		$error = "";
+
+		// check if its allowed to be empty
+		if (strlen($value) == 0) {
+			if (!$nullable) {
+				// display error saying this shouldnt be empty
+				$error = "The field $fieldName is not allowed to be empty";
+			} else {
+				// check if datetime
+				if ($type == "timestamp") {
+					$value = self::getSQLDateTime();
+				}
+			}
+		} else {
+			// proceed with length validation
+
+			// grab the length - expecting something like: varchar(11)
+			$start = stripos($type, "(") + 1;
+			$end = stripos($type, ")");
+			$maxLen = intval(substr($type, $start, $end - $start));
+
+			// check if value length exceeds maximum length
+			if ($maxLen != 0 && strlen($value) > $maxLen) {
+				// exceeds length diplay errror
+				$error = "The field $fieldName exceeds the maximum length of $maxLen";
+			} else {
+				// proceed with type validation
+
+				// grab the type
+				// check to see if there is a length passed
+				$end = stripos($type, "(");
+
+				// if not, then the end is the entire length
+				if (!$end) {
+					$end = strlen($type);
+				}
+
+				$type = substr($type, 0, $end);
+
+				$errType = "";
+
+				// switch based on type
+				switch($type) {
+					case "varchar" :
+					case "text" :
+					// check if its a string
+						if (!is_string($value)) {
+							$errType = "string";
+						}
+						break;
+
+					case "int" :
+					case 'tinyint' :
+						if (is_numeric($value)) {
+							// convert to integer
+							$value = intval($value);
+						} else {
+							// this should have been an integer
+							$errType = "number";
+						}
+						break;
+
+					case "timestamp" :
+					// Check if its a date time by converting to a unix time stamp
+						if (strtotime($value)) {
+							$value = self::getSQLDateTime($value);
+						} else {
+							$errType = "date and time value";
+						}
+						break;
+
+					default :
+						$errType = "Unexpected field type";
+						break;
+				}// switch
+
+				if (!empty($errType)) {
+					$error = "The field $fieldName should be a " . $errType;
+				}
+
+			} // else;
+		}
+
+		return $error;
+	}
+
+}
+?>
---
+++ projects/project3/search.php
@@ -0,0 +1,151 @@
+<?php
+
+define("BASE_URL", "http://simon.ist.rit.edu:8080/Services/resources/ESD/");
+
+//include any libraries/classes needed
+function __autoload($className) {
+	require_once 'classes/' . $className . '.class.php';
+}
+
+function buildHospitalSelect($path, $dataElem, $name_id, $type) {
+	// build select for Organization TYPE
+
+	// grab the dom
+	$dom = new DOMDocument();
+	$dom -> load(BASE_URL . $path);
+
+	// grab all the data nodes
+	$rows = $dom -> getElementsByTagName("$dataElem");
+
+	// setup array to store the information
+	$values = array();
+	$texts = array();
+
+	//loop thru rows
+	foreach ($rows as $row) {
+		// get the id
+		$data = $row -> nodeValue;
+
+		// push to respective arrays
+		$values[] = $data;
+		$texts[] = $data;
+	}
+
+	// add default value
+	array_unshift($texts, "All $type");
+	array_unshift($values, "");
+
+	// build select
+	// would like to put the IDs in the value, but our search needs the text
+	return Form::buildSelect($values, $texts, $name_id, $name_id);
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8" />
+		<title>search</title>
+		<meta name="description" content="" />
+		<meta name="author" content="Charles" />
+		<link rel="shortcut icon" href="img/pedro.ico" />
+		<!-- CSS -->
+		<link rel="stylesheet" href="css/form.css" />
+		<!-- 		<link type="text/css" href="css/vanilla_reset.css" /> -->
+		<!-- 		<link type="text/css" href="css/infieldLabel.css" /> -->
+		<!-- JavaScript -->
+		<!-- ************ PLUGINS **********************-->
+		<script type="text/javascript" src="plugins/jquery-1.7.1.min.js"></script>
+		<!-- in-field label plugin: http://fuelyourcoding.com/scripts/infield/ -->
+<!-- 		<script type="text/javascript" src="plugins/jquery.infieldlabel.min.js"></script> -->
+		<!-- ************ My Scripts **********************-->
+		<script type="text/javascript" src="js/page.js"></script>
+	</head>
+	<body>
+		<!-- search form -->
+		<form name=searchForm id="searchForm">
+			<fieldset>
+				<legend>
+					Search Criteria
+				</legend>
+				<p>
+					<label for="type">Organization Type</label>
+					<?php
+					// build select for Organization TYPE
+
+					// path: ...ESD/OrgTypes
+					$path = "OrgTypes";
+					$dataElem = "type";
+					$name = "type";
+					$default = "Organization Types";
+
+					// build the select and echo
+					echo buildHospitalSelect($path, $dataElem, $name, $default);
+					?>
+				</p>
+				<!-- build input for organization name -->
+				<p>
+					<label for="name">Organization Name</label>
+					<input type="text" name="name" />
+				</p>
+				<!-- Build input for state and City -->
+				<p>
+					<!-- Label -->
+					<label for="state">State</label>
+					<!-- State Combo -->
+					<?php
+					// build combo for state
+
+					// path: ...ESD/States
+					$path = "States";
+					$dataElem = "State";
+					$name = "state";
+					$default = "States";
+
+					// build the select and echo
+					echo buildHospitalSelect($path, $dataElem, $name, $default);
+					?>
+
+					<?php
+					// build combo for cities
+					// path: ...ESD/Cities
+					$path = "Cities";
+					$dataElem = "city";
+					$name = "city";
+					$default = "Cities";
+
+					// build the select and echo
+					echo buildHospitalSelect($path, $dataElem, $name, $default);
+					?>
+				</p>
+				<p>
+					<label for="county">County</label>
+					<?php
+					// combo for county
+
+					// path: ...ESD/Counties
+					$path = "Counties";
+					$dataElem = "CountyName";
+					$name = "county";
+					$default = "Counties";
+
+					// build the select and echo
+					echo buildHospitalSelect($path, $dataElem, $name, $default);
+					?>
+				</p>
+				<!-- input for zip code -->
+				<p>
+					<label for="zip">Zip Code</label>
+					<input type="text" name="zip" />
+				</p>
+				<!-- buttons -->
+				<div id="buttons">
+					<button type="button">
+						Show Results
+					</button>
+					<input type="reset" value="Reset Form" />
+				</div>
+			</fieldset>
+		</form>
+		<div id="tableOutput"></div>
+	</body>
+</html>
